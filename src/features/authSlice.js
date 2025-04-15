1886ebb692669dfc4b6b9f433130d9679f416c75
Bhavik-1602 <bhavikvala858@gmail.com>
2025-04-15 16:09:39 +0530

    modify

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // const API_URL = process.env.REACT_APP_API_URL;// Mock API endpoint
-const API_URL = `$process.env.REACT_APP_API_URL/api/auth`;
+const API_URL = `${process.env.REACT_APP_API_URL}/api/auth`;
 
 // Register user
 export const registerUser = createAsyncThunk("auth/registerUser", async (user, { rejectWithValue }) => {
